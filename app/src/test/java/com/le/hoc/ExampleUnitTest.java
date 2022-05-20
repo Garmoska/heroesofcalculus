cbc2fb40e4b254a639d58879bc265691527791c0
Leonov-Eugene <leonov.eugene@gmail.com>
2022-05-20 21:26:16 +0200

    assets

--- app/src/test/java/com/le/hoc/ExampleUnitTest.java
+++ app/src/test/java/com/le/hoc/ExampleUnitTest.java
@@ -15,26 +15,26 @@ import java.util.List;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+	@Test
+	public void addition_isCorrect() {
+		assertEquals(4, 2 + 2);
+	}
 
-    @Test
-    public void calculus_generation_isCorrect(){
-        final List<CalculusInfo> lst = CalculusFactory.prepareTasks(8, 4);
-        assertTrue(lst.size() > 0);
-        for (CalculusInfo calculusInfo: lst) {
-            assertNotNull(calculusInfo.getCalculus());
-            assertTrue(calculusInfo.getCorrectAnswer() > 0);
-            boolean found = false;
-            for (int a: calculusInfo.getAnswers()){
-                if (a == calculusInfo.getCorrectAnswer()){
-                    found = true;
-                    break;
-                }
-            }
-            assertTrue(found);
-        }
-    }
+	@Test
+	public void calculus_generation_isCorrect(){
+		final List<CalculusInfo> lst = CalculusFactory.prepareTasks(8, 4);
+		assertTrue(lst.size() > 0);
+		for (CalculusInfo calculusInfo: lst) {
+			assertNotNull(calculusInfo.getCalculus());
+			assertTrue(calculusInfo.getCorrectAnswer() > 0);
+			boolean found = false;
+			for (int a: calculusInfo.getAnswers()){
+				if (a == calculusInfo.getCorrectAnswer()){
+					found = true;
+					break;
+				}
+			}
+			assertTrue(found);
+		}
+	}
 }
