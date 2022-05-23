a6662226905492551b08410d78dabf29d4d7d1cf
Evgeny Leonov <eugene.leonov@gmail.com>
2022-05-23 14:49:50 +0200

    test check in

--- app/src/test/java/com/le/hoc/ExampleUnitTest.java
+++ app/src/test/java/com/le/hoc/ExampleUnitTest.java
@@ -10,7 +10,6 @@ import com.le.hoc.bl.CalculusInfo;
 import java.util.List;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
